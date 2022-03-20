71ded8005f895d53134fe84d5e44259e0ff6456e
Bajpai <BAJPASH3@novartis.net>
2022-03-20 18:10:57 +0530

    robber

---
+++ Dynamic Programming/house_robber_leetcode_m_213.py
@@ -0,0 +1,43 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, 
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight
+without alerting the police.
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Input: nums = [1,2,3]
+Output: 3
+'''
+
+import json
+
+nums = json.loads(input())
+
+# if len(nums) == 0:
+#     return 0
+# elif len(nums) == 1:
+#     return nums[0]
+# elif len(nums) == 2:
+#     return max(nums[0],nums[1])
+
+def rob(houses):
+    max_amount = {}
+    max_amount[0] = houses[0]
+    max_amount[1] = max(houses[0],houses[1])
+
+    for i in range(2,len(houses)):
+        # print(i)
+        max_amount[i] = max(max_amount[i-1], max_amount[i-2]+houses[i])
+    return max_amount[len(houses)-1]
+
+print(max(rob(nums[1:]),rob(nums[:-1])))
+
